c6b4cd266c188d931619197d2ef79ac1630ea476
Lisa Søndergaard <lisa3074@stud.kea.dk>
2020-10-05 13:02:57 +0200

    HTML is set up and inbox is styled in mobile view (with static data)

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   console.log("[Function] || init");
+  //lav en createElement img, i inbox template, hvis det er en gruppebesked => noget lignende:
+  /* if(image){
+    let el = document.createElement("img");
+    document.querySelector(".picture-wrapper").appendChild(el);
+  }else {
+      let el = document.createElement("div.no-picture");
+      let initials = document.createElement("p.initials");
+    document.querySelector(".picture-wrapper").appendChild(el);
+    document.querySelector(".no-picture").appendChild(initials);
+  }
+    */
 }
